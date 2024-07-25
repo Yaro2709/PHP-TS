e9f397158deb9f0590c7bc1fdc3297016cf17bb5
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2024-07-26 00:19:04 +0300

    Create is_object.ts

---
+++ var/is_object.ts
@@ -0,0 +1,7 @@
+module.exports = function is_object(mixedVar) {
+
+  if (Object.prototype.toString.call(mixedVar) === '[object Array]') {
+    return false
+  }
+  return mixedVar !== null && typeof mixedVar === 'object'
+}
