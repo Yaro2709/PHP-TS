484fb2b2e2532e096ae9b4a035e7eee989f3e888
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2024-07-26 00:13:20 +0300

    Create array_udiff_assoc.ts

---
+++ array/array_udiff_assoc.ts
@@ -0,0 +1,34 @@
+module.exports = function array_udiff_assoc(arr1) {
+
+  const retArr = {}
+  const arglm1 = arguments.length - 1
+  let cb = arguments[arglm1]
+  let arr = {}
+  let i = 1
+  let k1 = ''
+  let k = ''
+
+  const $global = typeof window !== 'undefined' ? window : global
+
+  cb =
+    typeof cb === 'string'
+      ? $global[cb]
+      : Object.prototype.toString.call(cb) === '[object Array]'
+        ? $global[cb[0]][cb[1]]
+        : cb
+
+  arr1keys: for (k1 in arr1) {
+    for (i = 1; i < arglm1; i++) {
+      arr = arguments[i]
+      for (k in arr) {
+        if (cb(arr[k], arr1[k1]) === 0 && k === k1) {
+          // If it reaches here, it was found in at least one array, so try next value
+          continue arr1keys  
+        }
+      }
+      retArr[k1] = arr1[k1]
+    }
+  }
+
+  return retArr
+}
