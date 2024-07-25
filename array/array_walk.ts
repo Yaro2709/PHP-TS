309612a2e679a7aac3471a372833725cd688dd4a
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2024-07-26 00:13:58 +0300

    Create array_walk.ts

---
+++ array/array_walk.ts
@@ -0,0 +1,24 @@
+module.exports = function array_walk(array, funcname, userdata) {
+
+  if (!array || typeof array !== 'object') {
+    return false
+  }
+
+  try {
+    if (typeof funcname === 'function') {
+      for (const key in array) {
+        if (arguments.length > 2) {
+          funcname(array[key], key, userdata)
+        } else {
+          funcname(array[key], key)
+        }
+      }
+    } else {
+      return false
+    }
+  } catch (e) {
+    return false
+  }
+
+  return true
+}
