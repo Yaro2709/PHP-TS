26ab5b83d151a6eeb8807c7fa64a5777f89bc858
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2024-07-26 00:11:29 +0300

    Create array_key_exists.ts

---
+++ array/array_key_exists.ts
@@ -0,0 +1,8 @@
+module.exports = function array_key_exists(key, search) {
+
+  if (!search || (search.constructor !== Array && search.constructor !== Object)) {
+    return false
+  }
+
+  return key in search
+}
