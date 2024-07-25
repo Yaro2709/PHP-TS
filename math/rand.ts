f505f823c71aa2c5d99aa3794f23b8c8d595b322
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2024-07-26 00:18:03 +0300

    Create rand.ts

---
+++ math/rand.ts
@@ -0,0 +1,11 @@
+module.exports = function rand(min, max) {
+
+  const argc = arguments.length
+  if (argc === 0) {
+    min = 0
+    max = 2147483647
+  } else if (argc === 1) {
+    throw new Error('Warning: rand() expects exactly 2 parameters, 1 given')
+  }
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
