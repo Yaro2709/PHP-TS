f6e9a4f743e93d1838285e57891f9aded08c5a70
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2024-07-26 00:10:54 +0300

    Create array_intersect.ts

---
+++ array/array_intersect.ts
@@ -0,0 +1,30 @@
+module.exports = function array_intersect(arr1) {
+
+  const retArr = {}
+  const argl = arguments.length
+  const arglm1 = argl - 1
+  let k1 = ''
+  let arr = {}
+  let i = 0
+  let k = ''
+
+  arr1keys: for (k1 in arr1) {
+    arrs: for (i = 1; i < argl; i++) {
+      arr = arguments[i]
+      for (k in arr) {
+        if (arr[k] === arr1[k1]) {
+          if (i === arglm1) {
+            retArr[k1] = arr1[k1]
+          }
+          // If the innermost loop always leads at least once to an equal value,
+          // continue the loop until done
+          continue arrs  
+        }
+      }
+      // If it reaches here, it wasn't found in at least one array, so try next value
+      continue arr1keys  
+    }
+  }
+
+  return retArr
+}
